872b181082b8173ddb5a526304c974e524c9fe0f
esysop <leehassel@gmail.com>
2016-03-09 16:10:04 -0600

    addition to rounds

--- Dice/src/BaseRound.java
+++ Dice/src/BaseRound.java
@@ -1,21 +1,16 @@
 
-public class Dice {
+public class BaseRound {
+
+	String roundName;
+	int roundNumber = 0;
+	int roundRank;
+	int currentRound;
+	int playerCount;
 	
-	int min = 1;
-	int max = 4;
-	int nextIntInRange(int min, int max, Random rng) {
-		   if (min > max) {
-		      throw new IllegalArgumentException("Cannot draw random int from invalid range [" + min + ", " + max + "].");
-		   }
-		   int diff = max - min;
-		   if (diff >= 0 && diff != Integer.MAX_VALUE) {
-		      return (min + rng.nextInt(diff + 1));
-		   }
-		   int i;
-		   do {
-		      i = rng.nextInt();
-		   } while (i < min || i > max);
-		   System.out.println(i);
-		   return i;
+		public int round()
+		{
+			//playerrank
+	
 		}
 }
+
